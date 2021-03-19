80318c7036e42c9f5b02d8a468495ae5a3b13993
devil_parents <evgeniy.webdesign@gmail.com>
2021-03-19 20:53:49 +0400

    Fix: Video stops  after 6 seconds of playback

--- public/assets/cms/js/youtube.js
+++ public/assets/cms/js/youtube.js
@@ -19,30 +19,5 @@ function onYouTubeIframeAPIReady() {
         height: videoHeight,
         width: videoWidth,
         videoId: videoId,
-        events: {
-            // 'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
     });
-}
-
-// 4. The API will call this function when the video player is ready.
-/*function onPlayerReady(event) {
-    event.target.playVideo();
-}*/
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-let done = false;
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-
-function stopVideo() {
-    player.stopVideo();
 }
